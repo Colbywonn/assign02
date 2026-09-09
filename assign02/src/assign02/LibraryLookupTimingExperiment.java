aab0a97096e9f70f46600852970daf1679566242
Colby <colbyjmiller@gmail.com>
2026-09-09 13:35:43 -0600

    Init timing experiment

---
+++ assign02/src/assign02/LibraryLookupTimingExperiment.java
@@ -0,0 +1,68 @@
+package assign02;
+
+import java.util.Random;
+
+/**
+ * Experiment to measure the running time for the lookup method in libraries of
+ * various sizes.
+ *
+ * @author CS 2420 course staff and Colby Miller & Todd Sorensen 
+ * @version September 9, 2026
+ */
+public class LibraryLookupTimingExperiment extends TimingExperiment {
+	private static String problemSizeDescription = "librarySize";  // TODO: fill in string appropriately
+	private static int problemSizeMin = 1000;  // TODO: initialize appropriately (do not use 0)
+	private static int problemSizeCount = 100000;  // TODO: initialize appropriately (do not use 0)
+	private static int problemSizeStep = 1000;  // TODO: initialize appropriately (do not use 0)
+	private static int experimentIterationCount = 100;
+	
+	private Library randomLibrary;
+	private long randomIsbn;
+	private static final Random rng = new Random();
+
+	public static void main(String[] args) {
+		TimingExperiment timingExperiment = new LibraryLookupTimingExperiment();
+
+		System.out.println("\n---Computing timing results---\n");
+		timingExperiment.printResults();
+	}
+
+	public LibraryLookupTimingExperiment() {
+		super(problemSizeDescription, problemSizeMin, problemSizeCount, problemSizeStep, experimentIterationCount);
+	}
+
+	/**
+	 * Populates the library with random library books.
+	 * 
+	 * @param problemSize - the size of the library
+	 */
+	@Override
+	protected void setupExperiment(int problemSize) {
+		randomLibrary = new Library();
+		for(int i = 0; i < problemSize; i++) {
+		    randomLibrary.add(generateIsbn(), "Surname", "Other name", "Title");
+		}
+		randomIsbn = generateIsbn();
+	}
+
+	/**
+	 * Runs the lookup method for the library.
+	 */
+	@Override
+	protected void runComputation() {
+		randomLibrary.lookup(randomIsbn);
+	}
+
+	/**
+	 * Generates a random ISBN (a long with 13 digits).
+	 *
+	 * @return randomly-generated ISBN
+	 */
+	private long generateIsbn() {
+		String isbn = "";
+		for(int j = 0; j < 13; j++) {
+		    isbn += rng.nextInt(10);
+		}
+		return Long.parseLong(isbn);
+	}
+}
