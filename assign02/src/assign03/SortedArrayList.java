410104dfc109d48895052f512acc1ba4ceaddc2c
Todd-Sorensen <todd.hyrum.sorensen@gmail.com>
2026-09-11 12:34:52 -0600

    Created files for assign03

---
+++ assign02/src/assign03/SortedArrayList.java
@@ -0,0 +1,89 @@
+package assign03;
+
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.NoSuchElementException;
+
+public class SortedArrayList<E> implements SortedList<E> {
+    
+    public SortedArrayList() {
+	
+    }
+    
+    public SortedArrayList(Comparator<? super E> cmp) {
+	
+    }
+    
+    @Override
+    public void clear() {
+	// TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public boolean contains(E element) {
+	// TODO Auto-generated method stub
+	return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection<? extends E> items) {
+	// TODO Auto-generated method stub
+	return false;
+    }
+
+    @Override
+    public int countEntries(E target) {
+	// TODO Auto-generated method stub
+	return 0;
+    }
+
+    @Override
+    public void insert(E element) {
+	// TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void insertAll(Collection<? extends E> coll) {
+	// TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public boolean isEmpty() {
+	// TODO Auto-generated method stub
+	return false;
+    }
+
+    @Override
+    public E max() throws NoSuchElementException {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+    @Override
+    public E median() throws NoSuchElementException {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+    @Override
+    public E min() throws NoSuchElementException {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+    @Override
+    public int size() {
+	// TODO Auto-generated method stub
+	return 0;
+    }
+
+    @Override
+    public Object[] toArray() {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+}
---
+++ assign02/src/assign03/SortedArrayListTest.java
@@ -0,0 +1,19 @@
+package assign03;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class SortedArrayListTest {
+    
+    @BeforeEach
+    void setUp() {
+	
+    }
+    @Test
+    void test() {
+	fail("Not yet implemented");
+    }
+
+}
